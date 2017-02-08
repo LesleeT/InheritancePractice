e39847bc014c6ded982eb2d2168baaa33ec25925
Leslee <Leslee@DESKTOP-273V837>
2017-02-08 10:33:31 -0600

    altered lab 4

---
+++ src/Furniture/StartUp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Furniture;
+
+/**
+ *
+ * @author Leslee
+ */
+public class StartUp {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Chair chair = new Chair();
+        chair.setFurnitureMaker("Broyhill");
+        System.out.println("The make of your chair is " + chair.getFurnitureMaker());
+       
+        
+        
+    }
+}
---
+++ src/Furniture/furniture.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Furniture;
+
+/**
+ *
+ * @author Leslee
+ */
+public class furniture {
+   private String furnitureMaker;
+
+    public String getFurnitureMaker() {
+        return furnitureMaker;
+    }
+
+    public void setFurnitureMaker(String furnitureMaker) {
+        this.furnitureMaker = furnitureMaker;
+    }
+   
+   
+}
