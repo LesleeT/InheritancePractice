94a19d488e1235ab6824f1a34f94ed48281d32f9
Leslee <Leslee@DESKTOP-273V837>
2017-02-08 14:23:03 -0600

    class example

---
+++ src/ClassExample/Circle.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ClassExample;
+
+/**
+ *
+ * @author ltrinastic
+ */
+public class Circle extends TwoDShape {
+    private double radius;
+
+    
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public double getArea(){
+        return Math.PI * radius * radius;
+    }
+    
+}
